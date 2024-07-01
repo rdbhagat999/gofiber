001aad34bc47431c2ae3496db84dbafd97dd46ac
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-07-01 22:45:13 +0530

    splits route handlers into a different file

--- main.go
+++ main.go
@@ -6,71 +6,33 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Post struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-}
-
-var postList []Post = []Post{
-	{Id: 1, Title: "First post"},
-	{Id: 2, Title: "Second post"},
-	{Id: 3, Title: "Third post"},
-}
-
-func getAllPosts(c *fiber.Ctx) error {
-	return c.JSON(postList)
-}
-
-func createPost(c *fiber.Ctx) error {
-	var post Post
-
-	err := c.BodyParser(&post)
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid data")
-	}
-
-	post.Id = len(postList) + 1
-
-	postList = append(postList, post)
-
-	return c.JSON(post)
-}
-
-func getPostById(c *fiber.Ctx) error {
-	id, err := c.ParamsInt("id")
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid id")
-	}
-
-	for _, item := range postList {
-		if item.Id == id {
-			return c.JSON(item)
-		}
-	}
-
-	return c.Status(fiber.StatusNotFound).SendString("Post not found")
+func CustomError(errCode int, message string) error {
+	customErr := fiber.NewError(errCode, message)
+	return customErr
 }
 
 func main() {
 	config := fiber.Config{
-		AppName: "Learn Golang with Fiber",
+		AppName:           "Learn Golang with Fiber",
+		EnablePrintRoutes: true,
+		Immutable:         false,
+		ServerHeader:      "Server 1",
 	}
 
 	app := fiber.New(config)
 
-	api := app.Group("/api")
+	api := app.Group("/api").Name("API ")
 
-	apiV1 := api.Group("/v1")
+	apiV1 := api.Group("/v1").Name("v1 ")
 
 	apiV1.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
-	})
+	}).Name("Hello World")
 
-	apiV1.Get("/posts", getAllPosts)
-	apiV1.Post("/posts", createPost)
-	apiV1.Get("/posts/:id", getPostById)
+	apiV1.Get("/posts", getAllPosts).Name("Get All Posts")
+	apiV1.Post("/posts", createPost).Name("Create New Post")
+	apiV1.Get("/author", getPostAuthor).Name("Get Post Author")
+	apiV1.Get("/posts/:id", getPostById).Name("Get Post By Id")
 
 	log.Fatal(app.Listen(":8080"))
 
